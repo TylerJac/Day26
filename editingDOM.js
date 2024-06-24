4a76b7b1b7b5ae575781ea9cadbb5e9d20264b4d
TylerJac <sans.gaming235@gmail.com>
2024-06-24 15:02:44 -0400

    Update editingDOM.js

--- editingDOM.js
+++ editingDOM.js
@@ -1,3 +1,39 @@
-const list = document.getElementById('list');
+const listItems = document.querySelectorAll('#list li');
 
-apps.innerHTML = "Granny Smith Apples"
+listItems.forEach((item) => {
+  if (item.innerHTML === 'Apples') {
+
+    item.innerHTML = 'Granny Smith Apples';
+  }
+});
+
+listItems.forEach((item) => {
+    if (item.innerHTML === 'Oat Milk') {
+  
+      item.remove();
+    }
+  });
+
+  const newLi = document.createElement('li');
+  newLi.innerHTML = 'Kombucha';
+
+  const list = document.querySelector('ul');
+
+  list.appendChild(newLi);
+
+  listItems.forEach((item) => {  item.remove();
+    }
+  );
+  list.innerHTML = '';
+
+  const newItems = ['protein bars', 'almonds', 'peanut butter'];
+  
+  newItems.forEach((item) => {
+    const li = document.createElement('li');
+    li.innerHTML = item;
+    if (item === 'almonds') {
+        li.classList.add('important');
+      }
+    list.appendChild(li);
+  });
+  
